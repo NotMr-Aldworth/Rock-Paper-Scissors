84835c4f698e674c9f5837b792924d72fde27556
cowajack474 <cowajack474@2260I-L42-76>
2022-04-21 11:00:25 -0400

    Minor Changes

--- Rock Paper Scissors/Form1.Designer.cs
+++ Rock Paper Scissors/Form1.Designer.cs
@@ -37,6 +37,8 @@
             this.radScissors = new System.Windows.Forms.RadioButton();
             this.grpUserchoice = new System.Windows.Forms.GroupBox();
             this.btnStart = new System.Windows.Forms.Button();
+            this.lblVs = new System.Windows.Forms.Label();
+            this.lblOutcome = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).BeginInit();
             this.grpUserchoice.SuspendLayout();
@@ -67,6 +69,7 @@
             // 
             this.txtUser.AutoSize = true;
             this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.SystemColors.Highlight;
             this.txtUser.Location = new System.Drawing.Point(126, 201);
             this.txtUser.Name = "txtUser";
             this.txtUser.Size = new System.Drawing.Size(61, 25);
@@ -77,6 +80,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
             this.label1.Location = new System.Drawing.Point(572, 201);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 25);
@@ -87,6 +91,7 @@
             // 
             this.radRock.AutoSize = true;
             this.radRock.Font = new System.Drawing.Font("Rockwell", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radRock.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.radRock.Location = new System.Drawing.Point(12, 12);
             this.radRock.Name = "radRock";
             this.radRock.Size = new System.Drawing.Size(73, 28);
@@ -100,6 +105,7 @@
             // 
             this.radPaper.AutoSize = true;
             this.radPaper.Font = new System.Drawing.Font("Goudy Stout", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radPaper.ForeColor = System.Drawing.SystemColors.Window;
             this.radPaper.Location = new System.Drawing.Point(12, 46);
             this.radPaper.Name = "radPaper";
             this.radPaper.Size = new System.Drawing.Size(104, 22);
@@ -113,6 +119,7 @@
             // 
             this.radScissors.AutoSize = true;
             this.radScissors.Font = new System.Drawing.Font("MS Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radScissors.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.radScissors.Location = new System.Drawing.Point(12, 74);
             this.radScissors.Name = "radScissors";
             this.radScissors.Size = new System.Drawing.Size(98, 20);
@@ -127,6 +134,7 @@
             this.grpUserchoice.Controls.Add(this.radPaper);
             this.grpUserchoice.Controls.Add(this.radScissors);
             this.grpUserchoice.Controls.Add(this.radRock);
+            this.grpUserchoice.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.grpUserchoice.Location = new System.Drawing.Point(64, 239);
             this.grpUserchoice.Name = "grpUserchoice";
             this.grpUserchoice.Size = new System.Drawing.Size(200, 100);
@@ -143,12 +151,39 @@
             this.btnStart.TabIndex = 8;
             this.btnStart.Text = "Play!";
             this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblVs
+            // 
+            this.lblVs.AutoSize = true;
+            this.lblVs.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblVs.Location = new System.Drawing.Point(371, 109);
+            this.lblVs.Name = "lblVs";
+            this.lblVs.Size = new System.Drawing.Size(48, 35);
+            this.lblVs.TabIndex = 9;
+            this.lblVs.Text = "VS";
+            this.lblVs.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblOutcome
+            // 
+            this.lblOutcome.AutoSize = true;
+            this.lblOutcome.Font = new System.Drawing.Font("Poor Richard", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutcome.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblOutcome.Location = new System.Drawing.Point(87, 381);
+            this.lblOutcome.Name = "lblOutcome";
+            this.lblOutcome.Size = new System.Drawing.Size(124, 24);
+            this.lblOutcome.TabIndex = 10;
+            this.lblOutcome.Text = "Who will win?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutcome);
+            this.Controls.Add(this.lblVs);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.grpUserchoice);
             this.Controls.Add(this.label1);
@@ -156,7 +191,7 @@
             this.Controls.Add(this.imgOpponent);
             this.Controls.Add(this.imgPlayer);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "RPS Game";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).EndInit();
@@ -178,6 +213,8 @@
         private System.Windows.Forms.RadioButton radScissors;
         private System.Windows.Forms.GroupBox grpUserchoice;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblVs;
+        private System.Windows.Forms.Label lblOutcome;
     }
 }
 
--- Rock Paper Scissors/Form1.cs
+++ Rock Paper Scissors/Form1.cs
@@ -41,8 +41,31 @@ namespace Rock_Paper_Scissors
         {
             imgPlayer.Image = Properties.Resources.istockphoto_186804075_1024x1024;
         }
-    
-        
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int random = generator.Next(1, 4);
+            if (random == 1)
+                imgOpponent.Image = Properties.Resources.dwayne_the_rock_johnson_gettyimages_1061959920;
+            else if (random == 2)
+                imgOpponent.Image = Properties.Resources.istockphoto_186804075_1024x1024;
+            else if (random == 3)
+                imgOpponent.Image = Properties.Resources.scissors_clip_art_cartoon_vector_1509551;
+            if (radRock.Checked && random == 1 || radPaper.Checked && random == 2 || radScissors.Checked && random == 3)
+                lblOutcome.Text = ("You have tied");
+            else if (radRock.Checked && random >= 2)
+                lblOutcome.Text = ("You have won!  Congratulations");
+
+
+
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
